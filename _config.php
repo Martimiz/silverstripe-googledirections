bb98ed2fd4cc96f676e03301791cb4aaf481e842
martimiz <martine@balbus.nl>
2014-06-24 17:58:08 +0200

    Disable GoogleDirections by default

--- _config.php
+++ _config.php
@@ -6,5 +6,5 @@ define('GOOGLEDIRECTIONS_BASE', basename(dirname(__FILE__)));
  * [GMap, link='My Link'] or [GMap, link="My Link"]This is a link[/GMap]
  * My Link being the name of the Map.
  */
-ShortcodeParser::get('default')->register('GMap', array('GoogleDirections', 'link_shortcode_handler'));
+// ShortcodeParser::get('default')->register('GMap', array('GoogleDirections', 'link_shortcode_handler'));
 
